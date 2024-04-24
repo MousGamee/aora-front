3132aa86727e8314294e1aaa49cef05ee936fcb3
Fatou Fofana <fatoufofana@MacBook-Air-de-Fatou.local>
2024-04-24 17:40:10 +0200

    create the tab navigation

---
+++ app/search/_layaout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const _layaout = () => {
+  return (
+    <View>
+      <Text>_layaout</Text>
+    </View>
+  )
+}
+
+export default _layaout
