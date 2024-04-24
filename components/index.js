f4069ad511d91c052d596c8d6123ad0946f41441
Fatou Fofana <fatoufofana@MacBook-Air-de-Fatou.local>
2024-04-24 23:13:46 +0200

    end of on g page

---
+++ components/index.js
@@ -0,0 +1,20 @@
+
+// import VideoCard from "./VideoCard";
+import FormField from "./FormField";
+import CustumButton from "./CustumButton";
+// import InfoBox from "./InfoBox";
+// import Loader from "./Loader";
+// import Trending from "./Trending";
+// import SearchInput from "./SearchInput";
+// import EmptyState from "./EmptyState";
+
+export {
+//   VideoCard,
+  FormField,
+  CustumButton,
+//   InfoBox,
+//   Loader,
+//   Trending,
+//   SearchInput,
+//   EmptyState,
+};
